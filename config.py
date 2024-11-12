b9f2067464ea3176e8424815ba189c0982e76b65
tdh <tdhster@gmail.com>
2024-11-12 17:46:55 +0800

    instruction and move some paramters in config.py from .env

--- config.py
+++ config.py
@@ -8,7 +8,19 @@ API_URL = config['API_URL']
 ALLOWED_PATH = config['ALLOWED_PATH']
 ERROR_MESSAGE_FOR_USER = config['ERROR_MESSAGE_FOR_USER']
 MANAGER_TELEGRAM_ID = config['MANAGER_TELEGRAM_ID']
-MIN_ASPECT_RATIO = float(config['MIN_ASPECT_RATIO'])
+MIN_ASPECT_RATIO = 0.67
 MAX_ASPECT_RATIO = 1 / MIN_ASPECT_RATIO
-BLURR_THRESHOLD = float(config['BLURR_THRESHOLD'])
-IMG_WORK_FORMAT = config['IMG_WORK_FORMAT'] 
+BLURR_THRESHOLD = 30.0
+IMG_WORK_FORMAT = 'jpg'
+SEND_AS_FILE_IOS = '''Для отправки фотографии как файл в телеграм, в максимальном исходном качестве на iPhone сделайте следующее:\n
+Нажмите скрепку в левом нижнем углу.\n
+Внизу нажмите кнопку «Файл».Нажмите «Выбрать из галереи».\n
+Выберете одно или несколько фото.\n
+Нажмите кнопку "Отправить".'''
+SEND_AS_FILE_ANDROID = '''Для отправки фотографии как файл в Telegram на устройстве Android, чтобы сохранить максимальное качество, выполните следующие шаги:
+Нажмите на скрепку в правом нижнем углу экрана.\n
+Выберите опцию «Файл».\n
+Нажмите «Выбрать из галереи», чтобы загрузить фото с устройства.\n
+Выберите одно или несколько фото.\n
+Нажмите кнопку "Отправить".\n
+'''
